cb2a6e3f09da688b12ebf8622fdc0d0c30ba0eca
ghaidaa24 <gfh1424@gmail.com>
2025-12-06 03:07:00 +0300

    Update caesar.py

--- algorithms/caesar.py
+++ algorithms/caesar.py
@@ -3,21 +3,21 @@
 
 import string
 
-def encrypt(plaintext, key):
+def encrypt(text, key):
     alphabet = string.ascii_lowercase
-    encrypted_word = ""
-    for letter in plaintext:
+    encrypted_text = ""
+    for letter in text:
         if letter in alphabet:
             original_position = alphabet.index(letter)
             new_position = (original_position + key) % 26
             new_letter = alphabet[new_position]
             if letter.isupper():
-                encrypted_word += new_letter.upper()
+                encrypted_text += new_letter.upper()
             else:
-                encrypted_word += new_letter
+                encrypted_text += new_letter
         else:
-            encrypted_word += letter
-    return encrypted_word
+            encrypted_text += letter
+    return encrypted_text
 
-def decrypt(plaintext, key):
-    return encrypt(plaintext, -key)
+def decrypt(text, key):
+    return encrypt(text, -key)
